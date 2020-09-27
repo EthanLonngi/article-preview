f7f29e03e3a255728cf9bb34a4500f02c5e443a7
Ethan Lonngi <ethanlonngi@gmail.com>
2020-09-27 11:54:51 -0500

    Change on share section

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
 const $shareIcon = document.querySelector(".share__icon");
 const $shareContent = document.querySelector(".share");
-const $info = document.querySelector(".info");
 const $shareColor = document.querySelector(".share__button");
 
 $shareIcon.addEventListener("click", () => {
   $shareIcon.classList.toggle("share__icon--active");
   $shareContent.classList.toggle("share--active");
-  $info.classList.toggle("info--active");
   $shareColor.classList.toggle("share__button--active");
 });
